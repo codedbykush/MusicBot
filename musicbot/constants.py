b912200360ad22acf9dce11749655e585adc8e4f
Vasco Evora <codedbykush@outlook.com>
2018-12-10 09:58:52 +0800

    Update constants.py

--- musicbot/constants.py
+++ musicbot/constants.py
@@ -1,7 +1,7 @@
 import os.path
 
-MAIN_VERSION = '1.9.8'
-SUB_VERSION = '_2'
+MAIN_VERSION = '0.0.1'
+SUB_VERSION = '_1'
 VERSION = MAIN_VERSION + SUB_VERSION
 
 AUDIO_CACHE_PATH = os.path.join(os.getcwd(), 'audio_cache')
